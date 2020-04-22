870ac95d6f74ca6cb44edf1f43a62d153b248338
Esteban <emedina@creatonico.com>
2020-04-22 23:05:15 +0200

    finish A-objects-intro

--- class/A-objects-intro/exercise-part-0.js
+++ class/A-objects-intro/exercise-part-0.js
@@ -5,3 +5,12 @@ Describe your own laptop as a JavaScript object
 Try to think of as many properties as you can!
 
 */
+
+let laptop = {
+  brand: "HP",
+  screenSize: 13,
+  isTouchscreen: false,
+};
+console.log(laptop.brand);
+console.log(laptop.screenSize);
+console.log(laptop.isTouchscreen);
--- class/A-objects-intro/exercise-part-1.js
+++ class/A-objects-intro/exercise-part-1.js
@@ -1,19 +1,47 @@
 /*
-
-Think of 5 different real world "things" that you 
+Think of 5 different real world "things" that you
 can describe with a JavaScript object
-
 Assign each of them to a separate variable
-
-
 For example:
-var car = {
+let car = {
     brand: "Seat",
     model: "Arona",
     color: "white",
     horsepower: 115,
     doors: 5
 };
-
 */
+
+let mobile = {
+    brand: "Apple",
+    model: "IPhone",
+    color: "white",
+    cameras: 2
+    proximity_sensor: true
+};
+
+
+let shirt = {
+    brand: "Zara",
+    model: "Man's Shirt",
+    color: "black",
+    size: 44,
+    long_sleeves: true
+};
+
+let coffee = {
+    brand: "Starbucks",
+    model: "Guatemala",
+    flavor: "strong",
+    quantity: kilos
+};
+
+
+let shoes = {
+    brand: "Nike",
+    model: "Air",
+    color: "black",
+    size: 41,
+    sport: true
+};
 
